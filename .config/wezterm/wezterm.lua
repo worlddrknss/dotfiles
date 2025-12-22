fd4dd0a290bd6c2870903606f98a00079a023584
WorldDrknss <cdshowalter2006@yahoo.com>
2025-12-22 10:00:21 -0600

    Background and Color Scheme Updated

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -76,8 +76,8 @@ config.background = {
                 path = wezterm.config_dir .. "/assets/background.gif",
             },
         },
-        width = "100%",
-        height = "100%",
+        horizontal_align = "Center",
+        vertical_align   = "Middle",
         opacity = 0.1,
     },
 }
@@ -103,5 +103,5 @@ config.max_fps = 120
 config.prefer_egl = true
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "RESIZE"
-config.color_scheme = 'darkmatrix'
+config.color_scheme = 'Chalk'
 return config
