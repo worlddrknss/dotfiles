40161d0a9a2b8d685ffa8691c141471cea2b22f4
WorldDrknss <cdshowalter2006@yahoo.com>
2026-01-16 22:56:20 -0600

    Add Neovim configuration and update Starship prompt with new styles and symbols

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -78,17 +78,17 @@ config.background = {
         },
         width = "100%",
         height = "100%",
-        opacity = 0.8,
+        opacity = 0.5,
     },
     {
         source = {
             File = {
-                path = wezterm.config_dir .. "/assets/background.gif",
+                path = wezterm.config_dir .. "/assets/background.jpg",
             },
         },
         horizontal_align = "Center",
         vertical_align   = "Middle",
-        opacity = 0.1,
+        opacity = 0.3,
     },
 }
 
@@ -112,6 +112,6 @@ config.window_frame = {
 config.max_fps = 120
 config.prefer_egl = true
 config.hide_tab_bar_if_only_one_tab = true
-config.window_decorations = "RESIZE"
-config.color_scheme = 'Chalk'
+-- config.window_decorations = "RESIZE"
+config.color_scheme = 'cyberpunk'
 return config
