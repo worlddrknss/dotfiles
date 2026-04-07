271c61c66d56c31e67747e472df07a79b4ddea5d
worlddrknss <cdshowalter2006@yahoo.com>
2026-04-06 19:10:38 -0500

    Refactor LSP configuration to improve structure and ensure automatic installation of additional language servers

--- .config/nvim/lua/worlddrknss/plugins/lsp.lua
+++ .config/nvim/lua/worlddrknss/plugins/lsp.lua
@@ -2,30 +2,48 @@
 -- LSP configuration for Neovim using mason and mason-lspconfig.
 -- ==============================================================
 return {
-    'folke/lazydev.nvim',
-    ft = 'lua',
-    opts = {
-        library = {{
-            path = '${3rd}/luv/library',
-            words = {'vim%.uv'}
-        }}
+    {
+        "folke/lazydev.nvim",
+        ft = "lua",
+        opts = {
+            library = {
+                {
+                    path = "${3rd}/luv/library",
+                    words = { "vim%.uv" },
+                },
+            },
+        },
+    },
+    {
+        "mason-org/mason.nvim",
+        opts = {},
     },
     {
         "mason-org/mason-lspconfig.nvim",
-        opts = {
-            ensure_installed = {
-                "lua_ls",
-                "bashls",
-                "gopls",
-                "html",
-                "cssls",
-                "angularls"
-            },
-            automatic_installation = true
-        },
-        dependencies = {{
+        dependencies = {
             "mason-org/mason.nvim",
-            opts = {}
-        }, "neovim/nvim-lspconfig"}
-    }
+            "neovim/nvim-lspconfig",
+        },
+        opts = function()
+            local ensure_installed = { "lua_ls" }
+
+            if vim.fn.executable("npm") == 1 then
+                vim.list_extend(ensure_installed, {
+                    "bashls",
+                    "html",
+                    "cssls",
+                    "angularls",
+                })
+            end
+
+            if vim.fn.executable("go") == 1 then
+                table.insert(ensure_installed, "gopls")
+            end
+
+            return {
+                ensure_installed = ensure_installed,
+                automatic_installation = true,
+            }
+        end,
+    },
 }
